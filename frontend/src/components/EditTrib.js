039f2c838b55584f93d0ed893892130c24a88387
Mayssa Ayachi <ayachi.mayssa@ensi-uma.tn>
2022-07-07 17:46:50 +0100

    tableau tribunaux

---
+++ frontend/src/components/EditTrib.js
@@ -0,0 +1,4 @@
+import React, { Fragment, useState } from "react";
+
+const EditDossier = ({dossier}) => {
+};
---
+++ frontend/src/components/ListTrib.js
@@ -0,0 +1,70 @@
+import React, { Fragment, useEffect, useState } from "react";
+import EditTrib from "./EditTrib";
+
+const ListDossier = () => {
+  const [trib, settrib] = useState([]);
+
+
+  const deletetrib = async id => {
+    try {
+      await fetch(`/tribunaux/list/${id}`, {
+        method: "DELETE"
+      });
+
+      settrib(dossiers.filter(trib => trib.trib_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const gettrib = async () => {
+    try {
+      const response = await fetch("/tribunaux/list");
+      const jsonData = await response.json();
+      
+      settrib(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    gettrib();
+  }, []);
+
+
+  return (
+    <Fragment>
+      {" "}
+      <table className="table table table-hover mt-5 text-center">
+        <thead  className="table-dark">
+          <tr>
+            <th>Tribunaux</th>
+            <th>Modifier</th>
+            <th>Supprimer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trib.map(trib => (
+            <tr key={trib.trib_id}>
+              <td>{trib.lieux}</td>
+              <td>
+                 
+              </td>
+              <td>
+              <button
+                  className="btn btn-danger"
+                  onClick={() => deletetrib(trib.trib_id)}
+                >
+                  Supprimer
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListDossier;
