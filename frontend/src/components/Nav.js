f0b5746fdc965862062ccdde301501aa241c1a94
Mayssa Ayachi <ayachi.mayssa@ensi-uma.tn>
2022-07-23 20:31:06 +0100

    rectif

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import {  Nav,Navbar, NavDropdown } from 'react-bootstrap'
+ import {  Nav,Navbar, NavDropdown } from 'react-bootstrap'
 
 
 function Navv() {
