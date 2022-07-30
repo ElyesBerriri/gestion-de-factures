cd99735602f248e9dd7a06e361cae3cd8b6b5d53
ElyesBerriri <ilyesberriri@gmail.com>
2022-07-30 14:19:36 +0100

    Update "services"

--- frontend/src/components/EditServices.js
+++ frontend/src/components/EditServices.js
@@ -1,8 +1,7 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 
-const EditServices= ({service}) => { 
+const EditServices = ({ service }) => {
 
   const [tribunal, settribunal] = useState(service.tribunal);
   const [nom, setnom] = useState(service.nom);
@@ -13,196 +12,179 @@ const EditServices= ({service}) => {
   const [vendredi, setvendredi] = useState(service.vendredi);
   const [samedi, setsamedi] = useState(service.samedi);
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-    const updatenom = async e => {
-      e.preventDefault();
-      {console.log(lundi)}
-
-      try {
-        const body = {tribunal,nom,lundi,mardi,mercredi,jeudi,vendredi,samedi };
-         await fetch(
-          `/services/list/${service.service_id}`,
-          {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body)
-          }
-        );
-  
-        window.location.reload();
-      } catch (err) {
-        console.error(err.message);
-      }
-    };
-  
-  
+  const updatenom = async e => {
+    e.preventDefault();
+    try {
+      const body = { tribunal, nom, lundi, mardi, mercredi, jeudi, vendredi, samedi };
+      await fetch(
+        `/services/list/${service.service_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
+        }
+      );
+      window.location.reload();
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   return (
-    <Fragment>
-      {console.log(lundi)}
+    <>
+      <Button variant="light" id="clbtnm" className="mx-3 disabled"
+        data-bs-toggle="modal"
+        data-bs-target={`#serr${service.service_id}`}
+        onClick={() => {
+          if (service.lundi === "Course") {
+            document.getElementById(`Courselun${service.service_id}`).checked = true;
+          }
+          else if (service.lundi === "Audience") {
+            document.getElementById(`Audiencelun${service.service_id}`).checked = true;
+          }
+          if (service.mardi === "Course") {
+            document.getElementById(`Coursemar${service.service_id}`).checked = true;
+          }
+          else if (service.mardi === "Audience") {
+            document.getElementById(`Audiencemar${service.service_id}`).checked = true;
+          }
+          if (service.mercredi === "Course") {
+            document.getElementById(`Coursemer${service.service_id}`).checked = true;
+          }
+          else if (service.mercredi === "Audience") {
+            document.getElementById(`Audiencemer${service.service_id}`).checked = true;
+          }
+          if (service.jeudi === "Course") {
+            document.getElementById(`Coursejeu${service.service_id}`).checked = true;
+          }
+          else if (service.jeudi === "Audience") {
+            document.getElementById(`Audiencejeu${service.service_id}`).checked = true;
+          }
+          if (service.vendredi === "Course") {
+            document.getElementById(`Courseven${service.service_id}`).checked = true;
+          }
+          else if (service.vendredi === "Audience") {
+            document.getElementById(`Audienceven${service.service_id}`).checked = true;
+          }
+          if (service.samedi === "Course") {
+            document.getElementById(`Coursesam${service.service_id}`).checked = true;
+          }
+          else if (service.samedi === "Audience") {
+            document.getElementById(`Audiencesam${service.service_id}`).checked = true;
+          }
+          settribunal(service.tribunal);
+          setnom(service.nom);
+          setlundi(service.lundi);
+          setmardi(service.mardi);
+          setmercredi(service.mercredi);
+          setjeudi(service.jeudi);
+          setvendredi(service.vendredi);
+          setsamedi(service.samedi);
+        }}>
+        Modifier
+      </Button>
 
-     <Button variant="light" id="clbtnm" className="mx-3 disabled" onClick={()=>{handleShow();
- setnom(service.nom);
- settribunal(service.tribunal);
-  if (lundi==="Course"){
-    document.getElementById(`Courselun${service.service_id}`).checked = true;
-  }
-  else if(lundi==="Audience"){
-    document.getElementById(`Audiencelun${service.service_id}`).checked = true;
-  }
-  if (mardi==="Course"){
-    document.getElementById(`Coursemar${service.service_id}`).checked = true;
-  }
-  else if(mardi==="Audience"){
-    document.getElementById(`Audiencemar${service.service_id}`).checked = true;
-  }
-  if (mercredi==="Course"){
-    document.getElementById(`Coursemer${service.service_id}`).checked = true;
-  }
-  else if(mercredi==="Audience"){
-    document.getElementById(`Audiencemer${service.service_id}`).checked = true;
-  }
-  if (jeudi==="Course"){
-    document.getElementById(`Coursejeu${service.service_id}`).checked = true;
-  }
-  else if(jeudi==="Audience"){
-    document.getElementById(`Audiencejeu${service.service_id}`).checked = true;
-  }
-  if (vendredi==="Course"){
-    document.getElementById(`Courseven${service.service_id}`).checked = true;
-  }
-  else if(vendredi==="Audience"){
-    document.getElementById(`Audienceven${service.service_id}`).checked = true;
-  }
-  if (samedi==="Course"){
-    document.getElementById(`Coursesam${service.service_id}`).checked = true;
-  }
-  else if(samedi==="Audience"){
-    document.getElementById(`Audiencesam${service.service_id}`).checked = true;
-  }
-}}>
-  Modifier
-  </Button>
-  
-  <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-
-<Modal.Header closeButton>
-          <Modal.Title>Modification d'une administration </Modal.Title>
-        </Modal.Header>
-    
-        <Modal.Body>
-      <div className="row">
-      <div className="input-group mb-3">
-      <span className="input-group-text text-secondary ">Tribunal :</span>
-
-       <input
-                type="text"
-                className="form-control"
-                 value={tribunal}
-                onChange={e => settribunal(e.target.value)}
-              />
+      <div className="modal fade" id={`serr${service.service_id}`} data-bs-backdrop="static">
+        <div className="modal-dialog modal-dialog-scrollable" >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title ">Modification d'un client</h4>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" />
             </div>
-      </div>
-       
-      <div className="row">
-      <div className="input-group mb-3">
-      <span className="input-group-text text-secondary ">Administration :</span>
+            <div className="modal-body">
+              <div className="row">
+                <div className="input-group mb-3">
+                  <span className="input-group-text text-secondary ">Tribunal :</span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={tribunal}
+                    onChange={e => settribunal(e.target.value)}
+                  />
+                </div>
+              </div>
 
-       <input
-                type="text"
-                className="form-control"
-                 value={nom}
-                onChange={e => setnom(e.target.value)}
-              />
+              <div className="row">
+                <div className="input-group mb-3">
+                  <span className="input-group-text text-secondary ">Administration :</span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={nom}
+                    onChange={e => setnom(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="table-responsive m-3 mytable mytable-41">
+                <table className="table table-hover text-center">
+                  <thead className="table-secondary text-secondary mytableheader">
+                    <tr>
+                      <th>Jour</th>
+                      <th>Course</th>
+                      <th>Audience</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Lundi</td>
+                      <td>
+                        <input type="radio"
+                          name="lun" id={`Courselun${service.service_id}`} onChange={() => setlundi("Course")} />
+                      </td>
+                      <td><input type="radio" name="lun" id={`Audiencelun${service.service_id}`} onChange={() => setlundi("Audience")} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Mardi</td>
+                      <td>  <input type="radio" name="mar" id={`Coursemar${service.service_id}`} onClick={() => setmardi("Course")} />
+                      </td>
+                      <td><input type="radio" name="mar" id={`Audiencemar${service.service_id}`} onClick={() => setmardi("Audience")} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Mercredi</td>
+                      <td>  <input type="radio" name="mer" id={`Coursemer${service.service_id}`} onClick={() => setmercredi("Course")} />
+                      </td>
+                      <td><input type="radio" name="mer" id={`Audiencemer${service.service_id}`} onClick={() => setmercredi("Audience")} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Jeudi</td>
+                      <td>  <input type="radio" name="j" id={`Coursejeu${service.service_id}`} onClick={() => setjeudi("Course")} />
+                      </td>
+                      <td><input type="radio" name="j" id={`Audiencejeu${service.service_id}`} onClick={() => setjeudi("Audience")} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Vendredi</td>
+                      <td>  <input type="radio" name="v" id={`Courseven${service.service_id}`} onClick={() => setvendredi("Course")} />
+                      </td>
+                      <td><input type="radio" name="v" id={`Audienceven${service.service_id}`} onClick={() => setvendredi("Audience")} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Samedi</td>
+                      <td>  <input type="radio" name="s" id={`Coursesam${service.service_id}`} onClick={() => setsamedi("Course")} />
+                      </td>
+                      <td><input type="radio" name="s" id={`Audiencesam${service.service_id}`} onClick={() => setsamedi("Audience")} />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
+            <div className="modal-footer">
+              <Button variant="light" id="valider"
+                onClick={e => updatenom(e)}
+              >Valider</Button>
+              <Button variant="dark" data-bs-dismiss="modal">Fermer</Button>
+            </div>
+          </div>
+        </div>
       </div>
-       
-  
-      <div className="table-responsive m-3 mytable mytable-41">
-        <table className="table table-hover text-center">
-          <thead className="table-secondary text-secondary mytableheader">
-
-          <tr>
-            <th>Jour</th>
-            <th>Course</th>
-            <th>Audience</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-
-            <tr>
-              <td>Lundi</td>
-              <td>  <input type="radio" 
-               name="lun" id={`Courselun${service.service_id}`} onChange={()=>setlundi("Course")} />
-             </td>
-             <td><input type="radio" name="lun" id={`Audiencelun${service.service_id}`} onChange={()=>setlundi("Audience")}/>
-             </td> 
-            </tr>
-            <tr>
-              <td>Mardi</td>
-              <td>  <input type="radio"  name="mar"  id={`Coursemar${service.service_id}`} onClick={()=>setmardi("Course")}/>
-             </td>
-             <td><input type="radio"  name="mar" id={`Audiencemar${service.service_id}`} onClick={()=>setmardi("Audience")}/>
-             </td> 
-            </tr>
-            <tr>
-              <td>Mercredi</td>
-              <td>  <input type="radio"  name="mer" id={`Coursemer${service.service_id}`} onClick={()=>setmercredi("Course")}/>
-             </td>
-             <td><input type="radio"  name="mer" id={`Audiencemer${service.service_id}`} onClick={()=>setmercredi("Audience")}/>
-             </td> 
-            </tr>
-            <tr>
-              <td>Jeudi</td>
-              <td>  <input type="radio"  name="j" id={`Coursejeu${service.service_id}`} onClick={()=>setjeudi("Course")}/>
-             </td>
-             <td><input type="radio"  name="j" id={`Audiencejeu${service.service_id}`} onClick={()=>setjeudi("Audience")}/>
-             </td> 
-            </tr>
-            <tr>
-              <td>Vendredi</td>
-              <td>  <input type="radio"  name="v" id={`Courseven${service.service_id}`} onClick={()=>setvendredi("Course")}/>
-             </td>
-             <td><input type="radio"  name="v" id={`Audienceven${service.service_id}`} onClick={()=>setvendredi("Audience")}/>
-             </td> 
-            </tr>
-            <tr>
-              <td>Samedi</td>
-              <td>  <input type="radio"  name="s" id={`Coursesam${service.service_id}`} onClick={()=>setsamedi("Course")}/>
-             </td>
-             <td><input type="radio"  name="s" id={`Audiencesam${service.service_id}`} onClick={()=>setsamedi("Audience")}/>
-             </td> 
-            </tr>
-          
-        </tbody>
-      </table>
-      </div>
-      </Modal.Body>
-
-              
-        <Modal.Footer>
-      <Button variant="light" id="valider" 
-      onClick={e => updatenom(e)}
-      >Valider</Button>
-      <Button variant="dark" data-bs-dismiss="modal">Fermer</Button>
-      </Modal.Footer>
-      
- 
-</Modal>
-
-     </Fragment>
-    );
-    
+    </>
+  );
 };
 
 export default EditServices;
--- frontend/src/routes/Services.js
+++ frontend/src/routes/Services.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import EditServices from "../components/EditServices";
 import InputService from "../components/InputService";
 import Button from 'react-bootstrap/Button';
@@ -8,29 +8,46 @@ const ListServices = () => {
   const [service, setservice] = useState([]);
   const [query, setQuery] = useState("");
   const [row, setRow] = useState("");
-  const [servicee, setservicee] = useState({});
-  const [id,setID]=useState("");
+  const [id, setID] = useState("");
+  const [servicee, setservicee] = useState({
+    "tribunal": "",
+    "nom": "",
+    "lundi": "",
+    "mardi": "",
+    "mercredi": "",
+    "jeudi": "",
+    "vendredi": "",
+    "samedi": ""
+  });
 
   const deleteservice = async id => {
     setRow("");
-      document.getElementById("clbtnm").className="btn btn-light mx-3 disabled";
-      document.getElementById("clbtnr").className="btn btn-dark disabled";
+    setservicee({
+      "tribunal": "",
+      "nom": "",
+      "lundi": "",
+      "mardi": "",
+      "mercredi": "",
+      "jeudi": "",
+      "vendredi": "",
+      "samedi": ""
+    });
+    document.getElementById("clbtnm").className = "btn btn-light mx-3 disabled";
+    document.getElementById("clbtnr").className = "btn btn-dark disabled";
     try {
       await fetch(`/services/list/${id}`, {
         method: "DELETE"
       });
-
       setservice(service.filter(service => service.service_id !== id));
     } catch (err) {
       console.error(err.message);
     }
   };
 
-  const getservice = async ()=> {
+  const getservice = async () => {
     try {
       const response = await fetch(`/services/list/?q=${query}`);
       const jsonData = await response.json();
-      
       setservice(jsonData);
     } catch (err) {
       console.error(err.message);
@@ -41,116 +58,110 @@ const ListServices = () => {
     getservice();
   }, [query]);
 
-
   return (
-    <Fragment >
-        
-        <h1  className='title' >Liste des Administrations</h1>
-
-        <div className="rechercheajout">
-          <InputService   />
-          <Search setQuery={(e) => setQuery(e.target.value.toLowerCase())} />
+    <>
+      <h1 className='title' >Liste des Administrations</h1>
+      <div className="rechercheajout">
+        <InputService />
+        <Search setQuery={(e) => setQuery(e.target.value.toLowerCase())} />
       </div>
- 
-
-      
       <div className="table-responsive mytable-56service">
         <table className="table table-hover text-center">
           <thead className="table-secondary text-secondary mytableheader">
-          <tr>
-            <th scope="col"> Tribunal</th>
-            <th scope="col">Nom</th>
-            <th scope="col">Lundi</th>
-            <th scope="col"> Mardi</th>
-            <th scope="col">Mercredi</th>
-            <th scope="col">Jeudi</th>
-            <th scope="col">Vendredi</th>
-            <th scope="col">Samedi</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {service.map(service => (
-            <tr key={service.service_id} id={`service${service.service_id}`} onClick={()=>{setID(service.service_id); 
-              setservicee(service);
-              let e = document.getElementById(`service${service.service_id}`);
-                  if (e.className !== "table-secondary") {
-                    if (row !== "") document.getElementById(row).className = "";
-                    e.className = "table-secondary";
-                    setRow(`service${service.service_id}`);
-                    document.getElementById("clbtnr").className="btn btn-dark";
-                    document.getElementById("clbtnm").className="btn btn-light mx-3";
-                   }}}>
-              <td >{service.tribunal}</td>
-              <td>{service.nom}</td>
-
-              <td className="back" style={
-                service.lundi=="Course"?
-                {'backgroundColor': '#868e96'} :
-                service.lundi=="Audience"?
-                {'backgroundColor': '#02c3cc'}:{'backgroundColor': '	#FFFFFF'}
-            }>{service.lundi}</td>
-              <td
-              style={
-                service.mardi=="Course"?
-                {'backgroundColor': '#868e96'}
-                :
-                service.mardi=="Audience"?
-                {'backgroundColor': '#02c3cc'}:{'backgroundColor': '	#FFFFFF'}
-            }>{service.mardi}</td>
-              <td
-              style={
-                service.mercredi=="Course"?
-                {'backgroundColor': "#868e96"}
-                :
-                service.mercredi=="Audience"?
-                {'backgroundColor': '#02c3cc'}:{'backgroundColor': '	#FFFFFF'}
-            }>{service.mercredi}</td>
-              <td
-              style={
-                service.jeudi=="Course"?
-                {'backgroundColor': '#868e96'}
-                :
-                service.jeudi=="Audience"?
-                {'backgroundColor': '#02c3cc'}:{'backgroundColor': '	#FFFFFF'}
-            }>{service.jeudi}</td>
-              <td
-           style={
-            service.vendredi=="Course"?
-            {'backgroundColor': '#868e96'}
-            :
-                service.vendredi=="Audience"?
-                {'backgroundColor': '#02c3cc'}:{'backgroundColor': '	#FFFFFF'}}
-            >{service.vendredi}</td>
-              <td
-              style={
-                service.samedi=="Course"?
-                {'backgroundColor': '#868e96'}
-                :
-                service.samedi=="Audience"?
-                {'backgroundColor': '#02c3cc'}:{'backgroundColor': '	#FFFFFF'}}>{service.samedi}</td>
-             
+            <tr>
+              <th scope="col"> Tribunal</th>
+              <th scope="col">Nom</th>
+              <th scope="col">Lundi</th>
+              <th scope="col"> Mardi</th>
+              <th scope="col">Mercredi</th>
+              <th scope="col">Jeudi</th>
+              <th scope="col">Vendredi</th>
+              <th scope="col">Samedi</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {service.map(service => (
+              <tr key={service.service_id} id={`service${service.service_id}`} onClick={() => {
+                setID(service.service_id);
+                setservicee(service);
+                let e = document.getElementById(`service${service.service_id}`);
+                if (e.className !== "table-secondary") {
+                  if (row !== "") document.getElementById(row).className = "";
+                  e.className = "table-secondary";
+                  setRow(`service${service.service_id}`);
+                  document.getElementById("clbtnr").className = "btn btn-dark";
+                  document.getElementById("clbtnm").className = "btn btn-light mx-3";
+                }
+              }}>
+                <td >{service.tribunal}</td>
+                <td>{service.nom}</td>
+                <td className="back" style={
+                  service.lundi == "Course" ?
+                    { 'backgroundColor': '#868e96' } :
+                    service.lundi == "Audience" ?
+                      { 'backgroundColor': '#02c3cc' } : { 'backgroundColor': '	#FFFFFF' }
+                }>{service.lundi}</td>
+                <td
+                  style={
+                    service.mardi == "Course" ?
+                      { 'backgroundColor': '#868e96' }
+                      :
+                      service.mardi == "Audience" ?
+                        { 'backgroundColor': '#02c3cc' } : { 'backgroundColor': '	#FFFFFF' }
+                  }>{service.mardi}</td>
+                <td
+                  style={
+                    service.mercredi == "Course" ?
+                      { 'backgroundColor': "#868e96" }
+                      :
+                      service.mercredi == "Audience" ?
+                        { 'backgroundColor': '#02c3cc' } : { 'backgroundColor': '	#FFFFFF' }
+                  }>{service.mercredi}</td>
+                <td
+                  style={
+                    service.jeudi == "Course" ?
+                      { 'backgroundColor': '#868e96' }
+                      :
+                      service.jeudi == "Audience" ?
+                        { 'backgroundColor': '#02c3cc' } : { 'backgroundColor': '	#FFFFFF' }
+                  }>{service.jeudi}</td>
+                <td
+                  style={
+                    service.vendredi == "Course" ?
+                      { 'backgroundColor': '#868e96' }
+                      :
+                      service.vendredi == "Audience" ?
+                        { 'backgroundColor': '#02c3cc' } : { 'backgroundColor': '	#FFFFFF' }}
+                >{service.vendredi}</td>
+                <td
+                  style={
+                    service.samedi == "Course" ?
+                      { 'backgroundColor': '#868e96' }
+                      :
+                      service.samedi == "Audience" ?
+                        { 'backgroundColor': '#02c3cc' } : { 'backgroundColor': '	#FFFFFF' }}>{service.samedi}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="modifsupp">
+        <EditServices service={servicee} />
+        <Button variant="dark" id="clbtnr" className="disabled" onClick={() => deleteservice(id)}>
+          Supprimer
+        </Button>
       </div>
 
-      <div className="modifsupp">
-        <EditServices service={servicee}  />
-      <Button variant="dark" id="clbtnr" className="disabled" onClick={() => deleteservice(id)}>
-      Supprimer
-        </Button>
-        </div>
-
-            <div className="courseaudience">
-            <h5 className="course" style={{'backgroundColor': '#868e96'
-    }}>Jour de Course</h5>
-    <h5 className="audience" style={{'backgroundColor': '#02c3cc'
-    }}>Jour d'Audience</h5>
-            </div>
-            
-     </Fragment>
+      <div className="courseaudience">
+        <h5 className="course" style={{
+          'backgroundColor': '#868e96'
+        }}>Jour de Course</h5>
+        <h5 className="audience" style={{
+          'backgroundColor': '#02c3cc'
+        }}>Jour d'Audience</h5>
+      </div>
+    </>
   );
 };
 
