45609c15e3365a2c570cd60d0df2576fc46c960a
ElyesBerriri <ilyesberriri@gmail.com>
2022-08-09 14:42:02 +0100

    Responsive background

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import back from "../gavel-scaled.jpg";
+import backp from "../gavel-scaledp.jpg";
 
 function Home() {
   return (
-   <img src={back} className="back"/>
+    <>
+      <img src={back} className="back" />
+      <img src={backp} className="backp" />
+    </>
   );
 }
 
