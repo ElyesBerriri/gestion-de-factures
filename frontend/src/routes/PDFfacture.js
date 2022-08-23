c1bc04d2d6e990a3f2dfba22f80447e1756ba5c9
ElyesBerriri <ilyesberriri@gmail.com>
2022-08-23 09:25:32 +0100

    Update PDFfacture.js

--- frontend/src/routes/PDFfacture.js
+++ frontend/src/routes/PDFfacture.js
@@ -50,76 +50,76 @@ function PDFfacture() {
             </div>
           </div>
 
-          <div style={{ margin: 'auto', marginTop: 0, padding:0,width: '20cm', border: '1px solid black' }}>
+          <div style={{ margin: 'auto', marginTop: 0, padding: 0, width: '20cm', border: '0.1mm solid black' }}>
             <div className="myRow">
-              <h6 className="col-12" style={{ border: '1px solid black',paddingRight: 0 }}>مذكرة أتعاب محاماة فاتورة عدد</h6>
+              <h6 className="col-12" style={{ border: '0.1mm solid black', paddingRight: 0 }}>مذكرة أتعاب محاماة فاتورة عدد</h6>
             </div>
             <div className="myRow">
-              <div className="col-6" style={{ textAlign: 'right', border: '1px solid black' }}></div>
-              <div className="col-6" style={{ textAlign: 'right', border: '1px solid black' }}>
-                <h6>: الحريف</h6>
-                <h6>: المعرف الجبائي</h6>
-                <h6>: العنوان</h6>
-                <h6>: الهاتف</h6>
-                <h6>: الايمايل</h6>
+              <div style={{ textAlign: 'right', border: '0.1mm solid black',width: '9.96cm' }}></div>
+              <div style={{ textAlign: 'right', border: '0.1mm solid black',width: '9.99cm' }}>
+                <h6>{total} : الحريف</h6>
+                <h6>{total} : المعرف الجبائي</h6>
+                <h6>{total} : العنوان</h6>
+                <h6>{total} : الهاتف</h6>
+                <h6>{total} : الايمايل</h6>
               </div>
             </div>
 
             <div className="myRow">
-              <h6 className="col-2" style={{ border: '1px solid black',paddingRight: 0 }}>المبلغ خارج الضريبة</h6>
-              <h6 className="col-4" style={{ border: '1px solid black',paddingRight: 0 }}>المرجع</h6>
-              <h6 className="col-5" style={{ border: '1px solid black',paddingRight: 0 }}>الموضوع</h6>
-              <h6 className="col-1" style={{ border: '1px solid black',paddingRight: 0 }}>رقم</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '3.56cm' }}>المبلغ خارج الضريبة</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '6.4cm' }}>المرجع</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '8.8cm' }}>الموضوع</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '1.19cm' }}>رقم</h6>
             </div>
             <div className="myRow">
-              <div className="col-2" style={{ border: '1px solid black' }}>
-                <h6 style={{paddingRight: 0}}>400.000</h6>
-                <h6 style={{paddingRight: 0}}>200.000</h6>
+              <div style={{ border: '0.1mm solid black',width: '3.56cm' }}>
+                <h6 style={{ paddingRight: 0 }}>400.000</h6>
+                <h6 style={{ paddingRight: 0 }}>200.000</h6>
               </div>
-              <div className="col-4" style={{ textAlign: 'right', border: '1px solid black' }}>
+              <div style={{ textAlign: 'right', border: '0.1mm solid black',width: '6.4cm' }}>
                 <h6>قضية عدد</h6>
                 <h6>قضية عدد</h6>
               </div>
-              <div className="col-5" style={{ textAlign: 'right', border: '1px solid black' }}>
+              <div style={{ textAlign: 'right', border: '0.1mm solid black',width: '8.8cm' }}>
                 <h6>أتعاب محاماة قضية مخالفة جبائية جزائية</h6>
                 <h6>أتعاب محاماة قضية مخالفة جبائية جزائية</h6>
               </div>
-              <div className="col-1" style={{ textAlign: 'right', border: '1px solid black' }}>
+              <div style={{ textAlign: 'right', border: '0.1mm solid black',width: '1.19cm' }}>
                 <h6>01</h6>
                 <h6>02</h6>{/*()?<h6>:<br>*/}
                 <br /><br /><br /><br />
               </div>
             </div>
-            <div className="myRow">
-              <h6 className="col-2" style={{ border: '1px solid black',paddingRight: 0 }}>600.000</h6>
-              <h6 className="col-4" style={{ border: '1px solid black',paddingRight: 0 }}>الموضوع</h6>
-              <h6 className="col-5" style={{ border: '1px solid black',paddingRight: 0 }}></h6>
-              <h6 className="col-1" style={{ border: '1px solid black',paddingRight: 0 }}></h6>
+            <div className="myRow" style={{ backgroundColor: '#BBA14A' }}>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '3.56cm' }}>600.000</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '6.4cm' }}>المجموع</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '8.8cm' }}></h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '1.19cm' }}></h6>
             </div>
             <div className="myRow">
-              <div className="col-2" style={{ border: '1px solid black' }}>
-                <div className="myRow" style={{display: 'block'}}>
-                  <h6 style={{ borderBottom: '1px solid black',paddingRight: 0 }}>78.000</h6>
+              <div style={{ border: '0.1mm solid black',width: '3.56cm' }}>
+                <div className="myRow" style={{ display: 'block'}}>
+                  <h6 style={{ borderBottom: '0.1mm solid black', paddingRight: 0 }}>78.000</h6>
                 </div>
-                <div className="myRow" style={{display: 'block'}}>
-                  <h6 style={{ borderBottom: '1px solid black',paddingRight: 0 }}>20.340</h6>
+                <div className="myRow" style={{ display: 'block' }}>
+                  <h6 style={{ borderBottom: '0.1mm solid black', paddingRight: 0 }}>20.340</h6>
                 </div>
-                <div className="myRow" style={{display: 'block'}}>
-                  <h6 style={{ borderBottom: '1px solid black',paddingRight: 0 }}>0.600</h6>
-                </div>
-              </div>
-              <div className="col-4" style={{ border: '1px solid black' }}>
-                <div className="myRow" style={{display: 'block'}}>
-                  <h6 style={{ textAlign: 'right', borderBottom: '1px solid black' }}>الاداء على القيمة المضافة</h6>
-                </div>
-                <div className="myRow" style={{display: 'block'}}>
-                  <h6 style={{ textAlign: 'right', borderBottom: '1px solid black' }}>الخصم من المورد</h6>
-                </div>
-                <div className="myRow" style={{display: 'block'}}>
-                  <h6 style={{ textAlign: 'right', borderBottom: '1px solid black'}}>الطابع الجبائي</h6>
+                <div className="myRow" style={{ display: 'block' }}>
+                  <h6 style={{ borderBottom: '0.1mm solid black', paddingRight: 0 }}>0.600</h6>
                 </div>
               </div>
-              <div className="col-6" style={{ textAlign: 'right', border: '1px solid black' }}>
+              <div style={{ border: '0.1mm solid black',width: '6.4cm' }}>
+                <div className="myRow" style={{ display: 'block' }}>
+                  <h6 style={{ textAlign: 'right', borderBottom: '0.1mm solid black',width: '6.4cm' }}>الاداء على القيمة المضافة</h6>
+                </div>
+                <div className="myRow" style={{ display: 'block' }}>
+                  <h6 style={{ textAlign: 'right', borderBottom: '0.1mm solid black',width: '6.4cm' }}>الخصم من المورد</h6>
+                </div>
+                <div className="myRow" style={{ display: 'block' }}>
+                  <h6 style={{ textAlign: 'right', borderBottom: '0.1mm solid black',width: '6.4cm' }}>الطابع الجبائي</h6>
+                </div>
+              </div>
+              <div style={{ textAlign: 'right', border: '0.1mm solid black',width: '9.99cm' }}>
                 <h6>: طريقة التسديد</h6>
                 <div className="myRow">
                   <div className="col-9" style={{ paddingRight: 0 }}>شاك</div>
@@ -147,12 +147,11 @@ function PDFfacture() {
                 </div>
               </div>
             </div>
-
-            <div className="myRow">
-              <input className="col-2" id="txt" style={{ border: '1px solid black', textAlign: 'center',fontSize: '0.39cm',fontWeight: 500,lineHeight: 1.2 }} value={total} readOnly />
-              <h6 className="col-4" style={{ border: '1px solid black',paddingRight: 0 }}>المبلغ الصافي</h6>
-              <h6 className="col-5" style={{ border: '1px solid black',paddingRight: 0 }}></h6>
-              <h6 className="col-1" style={{ border: '1px solid black',paddingRight: 0 }}></h6>
+            <div className="myRow" style={{ backgroundColor: '#BBA14A' }}>
+              <input id="txt" style={{ border: '0.1mm solid black', textAlign: 'center', fontSize: '0.39cm', fontWeight: 500, lineHeight: 1.2, backgroundColor: '#BBA14A',width: '3.56cm' }} value={total} readOnly />
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '6.4cm' }}>المبلغ الصافي</h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '8.8cm' }}></h6>
+              <h6 style={{ border: '0.1mm solid black', paddingRight: 0,width: '1.19cm' }}></h6>
             </div>
           </div>
           <div className="mt-3" style={{ minHeight: '2.2cm' }}>
@@ -163,7 +162,7 @@ function PDFfacture() {
             الإمضاء
           </div>
           <div className="pt-5">
-            <hr />
+            <hr style={{height:'2',color:'black',backgroundColor:'black',opacity: '1',height: '0.5mm'}} />
           </div>
           <h6>CENTRE ELKHALIL N°124 SLIMANE KAHYA MANOUBA 2010-TUNISIE</h6>
           <h6>Tél:(+216)50409407 / (+216)36442043 Fax:(+216)71601434 Email: lawyer.caat@gmail.com</h6>
